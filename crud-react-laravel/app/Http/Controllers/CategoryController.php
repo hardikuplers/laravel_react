7ce237da41cc9eb59c92e1f80250180a6d339674
Hardik Kotak <hardik.kotak@uplers.in>
2022-09-29 11:18:35 +0530

    testing

--- crud-react-laravel/app/Http/Controllers/CategoryController.php
+++ crud-react-laravel/app/Http/Controllers/CategoryController.php
@@ -12,7 +12,7 @@ class CategoryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function indexTest()
     {
         //
     }
