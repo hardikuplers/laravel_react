28924fc16593a45cf01ab84834212fa2bc1fcf5f
Hardik Kotak <hardik.kotak@uplers.in>
2022-10-20 15:27:52 +0530

    BE : product image changes

--- crud-react-laravel/app/Http/Controllers/ProductController.php
+++ crud-react-laravel/app/Http/Controllers/ProductController.php
@@ -20,7 +20,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return Product::select('id','title','description','image')->get();
+        return Product::select('id','title','description','image','created_at')->get();
     }
 
     /**
